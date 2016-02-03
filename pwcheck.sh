6f32b4fbbf0b1f989c41e155df684d9c9450300d
fieldn <fieldn@purdue.edu>
2016-02-03 16:01:42 -0500

    Lab 2 commit

--- pwcheck.sh
+++ pwcheck.sh
@@ -7,30 +7,38 @@ git commit -a -m "Lab 2 commit" >> .local.git.out
 #Your code here
 POINTS=0
 PASSWORD=$(cat $1)
+PWDSCORE="Password Score: "
 ERR="Error: Password length invalid."
 COUNT=${#PASSWORD}
 SPECIAL=$(egrep -c '[\#\$\+\%\@]' $1)
 NUMBER=$(egrep -c '[0-9]' $1)
 ALPHA=$(egrep -c '[A-Za-z]' $1)
+REPCHAR=$(egrep -c '[(A-Za-z)]' $1)
 
-echo $COUNT
+#password length is between 6 and 32
 if [ "$COUNT" -lt "6" ] || [ "$COUNT" -gt "32" ] ; then
     echo $ERR
+
 fi
 let POINTS=POINTS+COUNT
 echo $POINTS
 
-if [ "$SPECIAL" -gt "0" ] ; then    #password contains special characters
+#password contains special characters
+if [ "$SPECIAL" -gt "0" ] ; then    
     let POINTS=POINTS+5
 fi
 echo $POINTS
 
-if [ "$NUMBER" -gt "0" ] ; then    #password contains a number
+#password contains a number
+if [ "$NUMBER" -gt "0" ] ; then    
     let POINTS=POINTS+5
 fi
 echo $POINTS
 
-if [ "$ALPHA" -gt "0" ] ; then    #password contains an alpha character
+#password contains an alpha character
+if [ "$ALPHA" -gt "0" ] ; then    
     let POINTS=POINTS+5
 fi
 echo $POINTS
+
+echo $PWDSCORE + $POINTS
