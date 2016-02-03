55170739dde43419fe1d3a717351e10c54871df6
fieldn <fieldn@purdue.edu>
2016-02-03 16:46:30 -0500

    Lab 2 commit

--- pwcheck.sh
+++ pwcheck.sh
@@ -27,23 +27,30 @@ let POINTS=POINTS+COUNT
 
 #password contains special characters
 [[ "$SPECIAL" -gt "0" ]] && ((POINTS+=5))
+echo $POINTS
 
 #password contains a number
 [[ "$NUMBER" -gt "0" ]] && ((POINTS+=5))
+echo $POINTS
 
 #password contains an alpha character
 [[ "$ALPHA" -gt "0" ]] && ((POINTS+=5))
+echo $POINTS
 
 #password contains an the same alpha character in a row
 [[ "$REPCHAR" -gt "0" ]] && ((POINTS-=10))
+echo $POINTS
 
 #password contains 3 uppercase characters
 [[ "$REPUPPER" -gt "0" ]] && ((POINTS-=3))
+echo $POINTS
 
 #password contains 3 lowercase characters in a row
 [[ "$RELOWER" -gt "0" ]] && ((POINTS-=3))
+echo $POINTS
 
 #password contains 3 numbers in a row
 [[ "$REPNUM" -gt "0" ]] && ((POINTS-=3))
+echo $POINTS
 
 echo $PWDSCORE $POINTS
