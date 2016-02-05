c9311ba8027055c0699a5687474532315cb6a7ba
fieldn <fieldn@purdue.edu>
2016-02-05 10:43:07 -0500

    Lab 2 commit

--- monitor.sh
+++ monitor.sh
@@ -63,7 +63,7 @@ function generate_report {
     num_reports=$( ls $REPORTS_DIR | egrep -l '([0-9]{2}\.){2}([0-9]{4}\.)([0-9]{2}\.){2}[0-9]{2}')    
     #num_reports=0
     #$( grep -cE '([0-9]{2}.){2}([0-9]{4}.)(\1){2}[0-9]{2}' $(ls $REPORTS_DIR) )
-
+    echo num_reports
     #if [ "$num_reports" -ge "$MAXIMUM_REPORTS" ]; then
         file_to_delete=$(head $(ls $REPORTS_DIR) | egrep '([0-9]{2}\.){2}([0-9]{4}\.)([0-9]{2}\.){2}[0-9]{2}')
         #file_to_delete=$(ls $REPORTS_DIR | head )
