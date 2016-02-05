3a2535cc64d9d98aa3e72fa771ba97aecf625775
fieldn <fieldn@purdue.edu>
2016-02-04 23:16:07 -0500

    Lab 2 commit

--- pwcheck.sh
+++ pwcheck.sh
@@ -4,9 +4,7 @@
 git add $0 >> .local.git.out
 git commit -a -m "Lab 2 commit" >> .local.git.out
 
-POINTS=0
 PASSWORD=$(cat $1)
-ERR="Error: Password length invalid."
 COUNT=${#PASSWORD}
 SPECIAL=$(grep -Ec '[\#\$\+\%\@]' $1)
 NUMBER=$(grep -Ec '[0-9]' $1)
@@ -16,25 +14,24 @@ REPUPPER=$(grep -Ec '[A-Z]{3,}' $1)
 REPLOWER=$(grep -Ec '[a-z]{3,}' $1)
 REPNUM=$(grep -Ec '[0-9]{3,}' $1)
 
-#password length is between 6 and 32
-if [ "$COUNT" -lt "6" ] || [ "$COUNT" -gt "32" ] ; then
-    echo $ERR
+if [ "$COUNT" -lt "6" ] || [ "$COUNT" -gt "32" ] ; then #check pw length is between 6 and 32
+    echo "Error: Password length invalid."
     exit 0
 fi
-let POINTS=POINTS+COUNT
+POINTS=COUNT
 
-[[ "$SPECIAL"  -gt "0" ]] && ((POINTS+=5))  #password contains special characters
+[[ "$SPECIAL"  -gt "0" ]] && ((POINTS+=5))  #pw contains special characters
 
-[[ "$NUMBER"   -gt "0" ]] && ((POINTS+=5))  #password contains a number
+[[ "$NUMBER"   -gt "0" ]] && ((POINTS+=5))  #pw contains a number
 
-[[ "$ALPHA"    -gt "0" ]] && ((POINTS+=5))  #password contains an alpha character
+[[ "$ALPHA"    -gt "0" ]] && ((POINTS+=5))  #pw contains an alpha character
 
-[[ "$REPCHAR"  -gt "0" ]] && ((POINTS-=10)) #password contains an the same alpha character in a row
+[[ "$REPCHAR"  -gt "0" ]] && ((POINTS-=10)) #pw contains an the same alpha character in a row
 
-[[ "$REPUPPER" -gt "0" ]] && ((POINTS-=3))  #password contains 3 uppercase characters
+[[ "$REPUPPER" -gt "0" ]] && ((POINTS-=3))  #pw contains 3 uppercase characters
 
-[[ "$REPLOWER" -gt "0" ]] && ((POINTS-=3))  #password contains 3 lowercase characters in a row
+[[ "$REPLOWER" -gt "0" ]] && ((POINTS-=3))  #pw contains 3 lowercase characters in a row
 
-[[ "$REPNUM"   -gt "0" ]] && ((POINTS-=3))  #password contains 3 numbers in a row
+[[ "$REPNUM"   -gt "0" ]] && ((POINTS-=3))  #pw contains 3 numbers in a row
 
 echo "Password Score:" $POINTS
