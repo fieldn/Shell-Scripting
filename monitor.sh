3273bc0a3211796203cfc7714b542ce533ab069c
fieldn <fieldn@purdue.edu>
2016-02-14 17:24:10 -0500

    Lab 2 commit

--- monitor.sh
+++ monitor.sh
@@ -125,7 +125,7 @@ function display {
 
     file_to_display=$(ls $REPORTS_DIR | egrep '([0-9]{2}\.){2}([0-9]{4}\.)([0-9]{2}\.){2}[0-9]{2}' | tail -1)
     loc="\033[4F"
-    echo -e "$loc $(cat ./reports_dir/$file_to_display)"
+    echo -e "$loc$(cat ./reports_dir/$file_to_display)"
 }
 
 check_arguments $# $@
